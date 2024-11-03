2a799b9c83ddd850dcb7671a688be36958687604
ardszsantos <lokgamer01@gmail.com>
2024-11-03 14:34:44 -0300

    i might just

--- windowcapture.py
+++ windowcapture.py
@@ -1,98 +1,78 @@
 import numpy as np
 import win32gui, win32ui, win32con
-
+import cv2
 
 class WindowCapture:
 
-    # properties
-    w = 0
-    h = 0
-    hwnd = None
-    cropped_x = 0
-    cropped_y = 0
-    offset_x = 0
-    offset_y = 0
-
     # constructor
     def __init__(self, window_name=None):
-        # find the handle for the window we want to capture.
-        # if no window name is given, capture the entire screen
-        if window_name is None:
-            self.hwnd = win32gui.GetDesktopWindow()
-        else:
+        # Set properties
+        self.hwnd = None
+        self.window_name = window_name
+        self.w = 0
+        self.h = 0
+        self.cropped_x = 0
+        self.cropped_y = 0
+        self.offset_x = 0
+        self.offset_y = 0
+
+        # Find the window if a name is given, else default to full screen
+        if window_name:
             self.hwnd = win32gui.FindWindow(None, window_name)
             if not self.hwnd:
-                raise Exception('Window not found: {}'.format(window_name))
+                raise Exception(f'Window not found: {window_name}')
 
-        # get the window size
-        window_rect = win32gui.GetWindowRect(self.hwnd)
-        self.w = window_rect[2] - window_rect[0]
-        self.h = window_rect[3] - window_rect[1]
+            # Get window size and position
+            window_rect = win32gui.GetWindowRect(self.hwnd)
+            self.w = window_rect[2] - window_rect[0]
+            self.h = window_rect[3] - window_rect[1]
 
-        # account for the window border and titlebar and cut them off
-        border_pixels = 8
-        titlebar_pixels = 30
-        self.w = self.w - (border_pixels * 2)
-        self.h = self.h - titlebar_pixels - border_pixels
-        self.cropped_x = border_pixels
-        self.cropped_y = titlebar_pixels
+            # Account for window borders
+            border_pixels = 8
+            titlebar_pixels = 30
+            self.w -= (border_pixels * 2)
+            self.h -= titlebar_pixels + border_pixels
+            self.cropped_x = border_pixels
+            self.cropped_y = titlebar_pixels
 
-        # set the cropped coordinates offset so we can translate screenshot
-        # images into actual screen positions
-        self.offset_x = window_rect[0] + self.cropped_x
-        self.offset_y = window_rect[1] + self.cropped_y
+            # Calculate offsets to translate coordinates
+            self.offset_x = window_rect[0] + self.cropped_x
+            self.offset_y = window_rect[1] + self.cropped_y
+        else:
+            # Capture the entire screen if no window name provided
+            screen_rect = win32gui.GetWindowRect(win32gui.GetDesktopWindow())
+            self.w = screen_rect[2] - screen_rect[0]
+            self.h = screen_rect[3] - screen_rect[1]
 
     def get_screenshot(self):
-
-        # get the window image data
-        wDC = win32gui.GetWindowDC(self.hwnd)
+        # Capture full screen
+        wDC = win32gui.GetWindowDC(win32gui.GetDesktopWindow())
         dcObj = win32ui.CreateDCFromHandle(wDC)
         cDC = dcObj.CreateCompatibleDC()
         dataBitMap = win32ui.CreateBitmap()
         dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
         cDC.SelectObject(dataBitMap)
-        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
 
-        # convert the raw data into a format opencv can read
-        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        # Capture the screen image
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj, (0, 0), win32con.SRCCOPY)
+
+        # Convert to numpy array
         signedIntsArray = dataBitMap.GetBitmapBits(True)
-        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img = np.frombuffer(signedIntsArray, dtype='uint8')
         img.shape = (self.h, self.w, 4)
 
-        # free resources
+        # Free resources
         dcObj.DeleteDC()
         cDC.DeleteDC()
         win32gui.ReleaseDC(self.hwnd, wDC)
         win32gui.DeleteObject(dataBitMap.GetHandle())
 
-        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
-        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type() 
-        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
-        img = img[...,:3]
-
-        # make image C_CONTIGUOUS to avoid errors that look like:
-        #   File ... in draw_rectangles
-        #   TypeError: an integer is required (got type tuple)
-        # see the discussion here:
-        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        # Convert to BGR and drop the alpha channel
+        img = img[..., :3]
         img = np.ascontiguousarray(img)
 
+        # Crop to the window if a specific window was selected
+        if self.window_name:
+            img = img[self.offset_y:self.offset_y + self.h, self.offset_x:self.offset_x + self.w]
+
         return img
-
-    # find the name of the window you're interested in.
-    # once you have it, update window_capture()
-    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
-    @staticmethod
-    def list_window_names():
-        def winEnumHandler(hwnd, ctx):
-            if win32gui.IsWindowVisible(hwnd):
-                print(hex(hwnd), win32gui.GetWindowText(hwnd))
-        win32gui.EnumWindows(winEnumHandler, None)
-
-    # translate a pixel position on a screenshot image to a pixel position on the screen.
-    # pos = (x, y)
-    # WARNING: if you move the window being captured after execution is started, this will
-    # return incorrect coordinates, because the window position is only calculated in
-    # the __init__ constructor.
-    def get_screen_position(self, pos):
-        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
